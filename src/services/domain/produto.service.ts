266c6177ebeafc5ddfdaa595e40e1606bbc80bc7
Camil Soares <camilasoares1507@gmail.com>
2018-10-03 15:27:11 -0300

    Carregando imagens dos produtos

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "../../../node_modules/@angular/core";
 import { HttpClient } from "../../../node_modules/@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
-import { Observable } from "../../../node_modules/rxjs/Rx";
+import { Observable } from "rxjs/Rx";
 import { ProdutoDTO } from "../../models/produto.dto";
 
 @Injectable()
@@ -12,5 +12,10 @@ export class ProdutoService {
 
     findByCategoria(categoria_id: string) : Observable<ProdutoDTO[]> {
         return this.http.get<ProdutoDTO[]>(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    }
+
+    getSmallImageFromBucket(id: string) : Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+        return this.http.get(url, {responseType: 'blob'});
     }
 }
